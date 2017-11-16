3e51641968c45f2aa96cc309a1f2e37b602ff044
Answermouth <ansel.gamet@telecomnancy.net>
2017-11-16 22:03:22 +0100

    read file

--- Rncode/spmfFormat.R
+++ Rncode/spmfFormat.R
@@ -1,18 +1,17 @@
 tabPatient <- unique(data$NIPATIENT)
 
-write("", file = "C:/Users/Ansel/Desktop/hackaton/ans/test",append = FALSE, sep = "")
+file.remove("C:/Users/Ansel/Desktop/hackaton/ans/test", "C:/Users/Ansel/Desktop/hackaton/ans/datetimes.csv")
 
 for (i in 1:length(tabPatient)){
   
   user <- data[data$NIPATIENT == tabPatient[i],]
-  niserv <- user$NISERVICE
   datetime <- user$DATETIME
+  user <- user$NISERVICE
   tmp <- (1+integer(length(user)))*(-1)
-  rsl <- c(rbind(niserv,tmp))
-  #rsl <- c(rbind(datetime,niserv,tmp))
+  rsl <- c(rbind(user,tmp))
+  dt <- c(datetime)
   rsl <- c(rsl,-2)
   
   write(gsub(',', '', toString(rsl)), file = "C:/Users/Ansel/Desktop/hackaton/ans/test",append = TRUE, sep = " ")
-  
-  write(gsub(',', '', toString(datetime)), file = "C:/Users/Ansel/Desktop/hackaton/ans/datetimes.csv",append = TRUE, sep = ",")
+  write(gsub(',', '', toString(dt)), file = "C:/Users/Ansel/Desktop/hackaton/ans/datetimes.csv",append = TRUE, sep = ",")
 }
--- src/mainObject.java
+++ src/mainObject.java
@@ -1,5 +1,7 @@
 import ca.pfv.spmf.algorithms.sequentialpatterns.prefixspan.AlgoPrefixSpan;
 
+import java.io.*;
+
 /**
  * Created by Ansel on 16/11/2017.
  */
@@ -12,11 +14,34 @@ public class mainObject {
         algo.setMaximumPatternLength(5);
 
         try {
-            algo.runAlgorithm("C:\\Users\\Ansel\\Desktop\\hackaton\\ans\\test", 0.20, "C:\\Users\\Ansel\\Desktop\\hackaton\\b.txt");
+            //algo.runAlgorithm("C:\\Users\\Ansel\\Desktop\\hackaton\\ans\\test", 0.20, "C:\\Users\\Ansel\\Desktop\\hackaton\\b.txt");
             System.out.print("Finished mining");
+
+            File patternsFile = new File("C:\\Users\\Ansel\\Desktop\\hackaton\\b.txt");
+            FileReader fileReader = new FileReader(patternsFile);
+            BufferedReader bfreader = new BufferedReader(fileReader);
+            String line;
+            String[] firstSplit;
+            String[] pattern;
+
+            line = bfreader.readLine();
+            line = bfreader.readLine();
+            //while ((line = bfreader.readLine()) != null) {
+                firstSplit = line.split("( #SUP: )|( #SID: )");
+
+                System.out.println(firstSplit[0]);
+
+                pattern = firstSplit[0].substring(0, firstSplit[0].length()-3).split(" -1 ");
+
+                System.out.println(pattern[0]);
+                System.out.println(pattern[pattern.length-1]);
+            //}
+
+
         } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
 }
+
